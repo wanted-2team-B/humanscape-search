e30f37033c2dd29f210ef1a5a00883ee104455fa
yunji-cho <pplusmessage@gmail.com>
2022-05-21 23:19:58 +0900

    clinicalTrial.ts proxy 추가

--- src/services/clinicalTrial.ts
+++ src/services/clinicalTrial.ts
@@ -3,21 +3,20 @@ import { IClinicalTrial, IClinicalTrials } from '../types/clinicalTrial';
 
 let number = 0;
 
+const PROXY = window.location.hostname === 'localhost' ? '/B551182/diseaseInfoService/getDissNameCodeList' : '/proxy';
+
 export const getClinicalTrialData = async (searchText: string): Promise<IClinicalTrial[]> => {
   try {
-    const { data } = await axios.get<IClinicalTrials>(
-      `/B551182/diseaseInfoService/getDissNameCodeList?ServiceKey=${process.env.REACT_APP_API_KEY}`,
-      {
-        params: {
-          searchText,
-          numOfRows: 30,
-          pageNo: 1,
-          sickType: 1,
-          diseaseType: 'SICK_NM',
-          medTp: 2,
-        },
-      }
-    );
+    const { data } = await axios.get<IClinicalTrials>(`${PROXY}?ServiceKey=${process.env.REACT_APP_API_KEY}`, {
+      params: {
+        searchText,
+        numOfRows: 30,
+        pageNo: 1,
+        sickType: 1,
+        diseaseType: 'SICK_NM',
+        medTp: 2,
+      },
+    });
     const emptyArr: IClinicalTrial[] = [];
     const { items, totalCount } = data.response.body;
     const responseItem = items.item;
