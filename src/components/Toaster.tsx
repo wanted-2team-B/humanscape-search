8aa09c8cd24f9b93dd3c933f4606980c054918de
doki333 <duckky333@gmail.com>
2022-05-21 18:28:15 +0900

    fix: toaster font-color

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 
 export const StyledToaster = () => {
-  const isDesktop = window.innerWidth >= 1000;
+  const isDesktop = window.innerWidth > 1000;
   return (
     <Toaster
       position={isDesktop ? 'top-center' : 'bottom-center'}
       toastOptions={{
         style: {
           background: '#f8d7da',
+          color: '#8C2A2F',
         },
       }}
     />
