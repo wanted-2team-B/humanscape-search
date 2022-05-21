7f20638054ee753b7b354dfce80ffe8323918dd1
yunji-cho <pplusmessage@gmail.com>
2022-05-21 22:18:02 +0900

    feat: 정규식 eslint 끄기 추가

--- src/libs/checkWord.ts
+++ src/libs/checkWord.ts
@@ -1,4 +1,5 @@
 export const checkWord = (keyword: string): boolean => {
+  // eslint-disable-next-line
   const pattern = /([^가-힣A-Za-z0-9\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"\s\x20])/i;
   if (pattern.test(keyword)) {
     return false;
